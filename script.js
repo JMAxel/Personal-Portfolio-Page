ef8baf1bd8769e3171a289806022aac185fe084e
JM <leugim3005@gmail.com>
2024-07-04 19:23:41 -0300

    Menu and improvements. Certificate next

--- script.js
+++ script.js
@@ -1,22 +1,61 @@
 const	aboutDescription = document.getElementById('about-description-id');
+const	header = document.getElementById('header-id');
+let		headerButton = document.getElementById('header-button');
+const	overlay = document.getElementById('overlay');
+const	sideMenu = document.getElementById('small-nav');
 
-const	checkWindowSize = () =>
+const	manageMenu = () =>
 {
-	let	width = window.innerWidth;
+	sideMenu.classList.toggle('close');
+	overlay.classList.toggle('close');
+}
+
+const	checkWindowSizeNav = () =>
+{
+	let 	width = window.innerWidth;
+
+	if (width > 850)
+	{
+		if (headerButton)
+		{
+			header.removeChild(headerButton);
+			headerButton = 0;
+		}
+	}
+	else if (width <= 850 && !headerButton)
+	{
+		//Prepare button
+		let newButton = document.createElement('button');
+		newButton.classList.add('nav-button');
+		newButton.id = 'header-button';
+
+		let menu = document.createElement('i');
+		menu.classList.add('material-symbols-outlined');
+		menu.classList.add('button-icon');
+		menu.innerHTML = 'menu';
+
+		newButton.appendChild(menu);
+
+		header.appendChild(newButton);
+		headerButton = document.getElementById('header-button');
+		headerButton.addEventListener('click', manageMenu);
+	}
+}
+
+const	checkWindowSizeLogo = () =>
+{
+	let		width = window.innerWidth;
 	const	aboutLogo = document.getElementById('about-logo-id');
 
-	console.log('Largura:', width);
 	if (width <= 1630)
 	{
 		if (aboutLogo)
 		{
-			console.log('Entra');
 			aboutDescription.removeChild(aboutLogo);
 		}
 	}
 	else if (width > 1630 && !aboutLogo)
 	{
-		console.log('Esta merda não funfa');
 		let	newDiv = document.createElement('div');
 		newDiv.classList.add('about-logo');
 		newDiv.id = 'about-logo-id';
@@ -30,8 +69,24 @@ const	checkWindowSize = () =>
 
 		aboutDescription.appendChild(newDiv);
 	}
-} 
+}
 
-window.addEventListener('resize', checkWindowSize);
+window.addEventListener('resize', checkWindowSizeLogo);
+window.addEventListener('resize', checkWindowSizeNav);
 
-checkWindowSize();
+headerButton.addEventListener('click', manageMenu);
+
+document.addEventListener('click', (event) =>
+{
+	if (sideMenu.classList.contains('close'))
+	{
+		if (overlay.contains(event.target))
+		{
+			sideMenu.classList.toggle('close');
+			overlay.classList.toggle('close');
+		}
+	}
+});
+
+checkWindowSizeLogo();
+checkWindowSizeNav();
